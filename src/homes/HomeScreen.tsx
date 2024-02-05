cba4bac529776f3e9e72cb4bc1a03d0ccc56f56e
ANPHATPLASTIC\cntt.ptda4 <leanhquyen1211@gmail.com>
2024-02-05 16:37:41 +0700

    add tag component

--- src/homes/HomeScreen.tsx
+++ src/homes/HomeScreen.tsx
@@ -8,14 +8,20 @@ import TextComponent from '../components/TextComponent';
 import TitleComponent from '../components/TitleComponent';
 import {colors} from '../constants/colors';
 import CardComponent from '../components/CardComponent';
+import {Element4, Notification, SearchNormal1} from 'iconsax-react-native';
+import IonIcons from 'react-native-vector-icons/Ionicons';
+import TagComponent from '../components/TagComponent';
+import SpaceComponent from '../components/SpaceComponent';
+import moment from 'moment';
 
 const HomeScreen = () => {
   return (
     <Container>
       <SectionComponent>
         <RowComponent justify="space-between">
-          <TextComponent text="fafa" />
-          <TextComponent text="fafa" />
+          <Element4 size={24} color={colors.desc} />
+          <Notification size={24} color={colors.desc} />
+          {/* <IonIcons name='notifications' size={24} color={colors.desc} /> */}
         </RowComponent>
       </SectionComponent>
       <SectionComponent>
@@ -26,19 +32,26 @@ const HomeScreen = () => {
         <RowComponent
           styles={[globalStyles.inputContainer]}
           onPress={() => Alert.alert('Welcome!!!')}>
-          <TextComponent text="Search" />
-          <Text>S</Text>
+          <TextComponent text="Search task" color="#696B6F" />
+          <SearchNormal1 size={28} color={colors.desc} />
         </RowComponent>
       </SectionComponent>
       <SectionComponent>
         <CardComponent>
           <RowComponent>
             <View style={{flex: 1}}>
-              <TitleComponent text='Task progress' />
-              <TextComponent text='30/40 tasks done' />
+              <TitleComponent text="Task progress" />
+              <TextComponent text="30/40 tasks done" />
+              <SpaceComponent height={12} />
+              <RowComponent justify="flex-start">
+                <TagComponent
+                  text={moment().format('MMMM DD')}
+                  onPress={() => console.log('Hello!!!')}
+                />
+              </RowComponent>
             </View>
             <View>
-              <TextComponent text='CircleChar' />
+              <TextComponent text="CircleChar" />
             </View>
           </RowComponent>
         </CardComponent>
